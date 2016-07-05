2ba91d585129a6817b26fa4e3ade5e5dcdec61c1
dochonglo <ismaelbarry777@hotmail.com>
2016-07-05 12:21:01 -0400

    Variable and Constants Lab Complete

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -4,9 +4,9 @@ print(favoriteCharacter)
 favoriteCharacter = "Tyrion Lannister"
 print(favoriteCharacter)
 
-favoriteCharacter = 76
+favoriteCharacter = "Liam Neeson"
 
 let ultimateFavoriteCharacter = "Arya Stark"
 print(ultimateFavoriteCharacter)
 
-ultimateFavoriteCharacter = "Daenerys Targaryen"
+//ultimateFavoriteCharacter = "Daenerys Targaryen" -> Does not work because this is a constant. 
